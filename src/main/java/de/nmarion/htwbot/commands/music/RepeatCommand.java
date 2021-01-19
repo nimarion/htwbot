6e4ed7015b8f9195bce17570702b667facbeb9f2
Niklas Marion <niklas.marion@gmx.de>
2021-01-19 14:40:47 +0000

    feat: add repeat command

---
+++ src/main/java/de/nmarion/htwbot/commands/music/RepeatCommand.java
@@ -0,0 +1,32 @@
+package de.nmarion.htwbot.commands.music;
+
+import de.nmarion.htwbot.commands.Command;
+import de.nmarion.htwbot.utils.DiscordUtils;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.Message;
+
+public class RepeatCommand extends Command {
+
+    public RepeatCommand() {
+        super("repeat", "Widerholt die Musik");
+    }
+
+    @Override
+    public void execute(String[] args, Message message) {
+        final EmbedBuilder embedBuilder = getEmbed(message.getGuild(), message.getAuthor());
+        if (DiscordUtils.isConnected(message.getMember(), embedBuilder)) {
+            if (message.getGuild().getAudioManager().getConnectedChannel() == null) {
+                message.getGuild().getAudioManager()
+                        .openAudioConnection(message.getMember().getVoiceState().getChannel());
+            }
+            getBot().getMusicManager().setRepeat(message.getGuild(),
+                    !getBot().getMusicManager().isRepeat(message.getGuild()));
+            embedBuilder.setDescription(
+                    getBot().getMusicManager().isRepeat(message.getGuild()) ? "Musik wird jetzt wiederholt :repeat:"
+                            : "Musik spielt jetzt wieder normal");
+        }
+        message.getTextChannel().sendMessage(embedBuilder.build()).queue();
+
+    }
+
+}
